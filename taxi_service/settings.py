bac5ae32ac0690831cff3890b48ddd52e66cd4f8
Artem D <OkeNNV@gmail.com>
2026-09-22 20:10:38 +0300

    required changes

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -121,7 +121,7 @@ STATIC_URL = 'static/'
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
 
-MAILERS = {
+EMAIL = {
     'default': {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
